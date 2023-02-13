a765ab6b1d0f5192f41b042c5647813ca33eb387
Justin Song <justinsong1608@gmail.com>
2023-02-13 22:56:51 +0000

    Added a submit event listener. #1

--- js/main.js
+++ js/main.js
@@ -6,3 +6,20 @@ function inputImage(event) {
 }
 
 $input.addEventListener('input', inputImage);
+
+var $journal = document.querySelector('form');
+
+function save(event) {
+  event.preventDefault();
+  var list = [];
+  list.title = $journal.elements.title.value;
+  list.photo = $journal.elements.photo.value;
+  list.notes = $journal.elements.notes.value;
+  list.entryId = data.nextEntryId;
+  data.nextEntryId += 1;
+  data.entries.push(list);
+  $img.setAttribute('src', '/images/placeholder-image-square.jpg');
+  $journal.reset();
+}
+
+$journal.addEventListener('submit', save);
